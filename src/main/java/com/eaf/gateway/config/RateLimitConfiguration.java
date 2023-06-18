1e82a569b9b88166ee1150227da71ec310a51941
Yogesh More <moreyog@gmail.com>
2023-06-18 14:05:36 +0530

    rate limit redis working

---
+++ src/main/java/com/eaf/gateway/config/RateLimitConfiguration.java
@@ -0,0 +1,18 @@
+package com.eaf.gateway.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class RateLimitConfiguration {
+
+    @Bean
+    public KeyResolver userKeyResolver() {
+        return exchange -> Mono.just("1");
+    }
+}
